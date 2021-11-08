c52861c7100252d73b26c3bcde3babdfc1e973ab
Seth Setse <seth@sethsetse.com>
2021-11-07 20:31:49 -0500

    New Line

--- document_parser.py
+++ document_parser.py
@@ -9,9 +9,11 @@ class DocumentParser:
         self.text = text
         self.doc = self.nlp(text)
         for i, sentence in enumerate(self.doc.sentences):
+            print(f'====== Sentence {i+1} Words =======')
             for word in sentence.words:
                 print(
                     f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}')
+            print("\n")
             # print(f'====== Sentence {i+1} tokens =======')
             # print(
             #     *[f'id: {token.id}\ttext: {token.text}' for token in sentence.tokens], sep='\n')
