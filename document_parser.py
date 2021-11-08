d7de66153c678bf10e9aefa9e3c8209e8cf565a3
Seth Setse <seth@sethsetse.com>
2021-11-07 20:30:23 -0500

    Print tokens

--- document_parser.py
+++ document_parser.py
@@ -3,12 +3,15 @@ stanza.download('en')
 
 
 class DocumentParser:
-    nlp = stanza.Pipeline(lang='en', processors='tokenize')
+    nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos')
 
     def __init__(self, text):
         self.text = text
         self.doc = self.nlp(text)
         for i, sentence in enumerate(self.doc.sentences):
-            print(f'====== Sentence {i+1} tokens =======')
-            print(
-                *[f'id: {token.id}\ttext: {token.text}' for token in sentence.tokens], sep='\n')
+            for word in sentence.words:
+                print(
+                    f'word: {word.text}\tupos: {word.upos}\txpos: {word.xpos}\tfeats: {word.feats if word.feats else "_"}')
+            # print(f'====== Sentence {i+1} tokens =======')
+            # print(
+            #     *[f'id: {token.id}\ttext: {token.text}' for token in sentence.tokens], sep='\n')
