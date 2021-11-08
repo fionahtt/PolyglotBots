bdbcfcc30ca2fe9e612b4f6196f476f9ecd4a8d2
Seth Setse <seth@sethsetse.com>
2021-11-07 20:24:10 -0500

    Parse sentences

--- document_parser.py
+++ document_parser.py
@@ -1,3 +1,14 @@
+import stanza
+stanza.download('en')
+
+
 class DocumentParser:
+    nlp = stanza.Pipeline(lang='en', processors='tokenize')
+
     def __init__(self, text):
         self.text = text
+        self.doc = self.nlp(text)
+        for i, sentence in enumerate(self.doc.sentences):
+            print(f'====== Sentence {i+1} tokens =======')
+            print(
+                *[f'id: {token.id}\ttext: {token.text}' for token in sentence.tokens], sep='\n')
--- question_answer.py
+++ question_answer.py
@@ -1,1 +1,4 @@
 import stanza
+from document_parser import DocumentParser
+
+parser = DocumentParser("I like pie. I like fat pies")
